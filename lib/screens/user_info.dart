9d5a0f5a541e0054900ac5c0c05839cc4501f8d1
nishi-2400 <a7w21087bump@yahoo.co.jp>
2021-08-14 19:50:43 +0900

    Bottom Bar作成＋スクリーンの切り替え

--- lib/screens/user_info.dart
+++ lib/screens/user_info.dart
@@ -4,8 +4,57 @@ class UserInfoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('User Info'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: userTitle('User Info'),
+          ),
+          Divider(
+            thickness: 1,
+            color: Colors.grey,
+          ),
+          userListTile(context, 'Email', 'Email sub', 0),
+          userListTile(context, 'Phone number', 'SubTitle', 1),
+          userListTile(context, 'Shipping address', 'SubTitle', 2),
+          userListTile(context, 'Joined date', 'date', 3),
+          userListTile(context, 'Home', 'SubTitle', 4),
+        ],
+      ),
+    );
+  }
+
+  List<IconData> _userTitleIcons = [
+    Icons.email,
+    Icons.phone,
+    Icons.local_shipping,
+    Icons.watch_later,
+    Icons.exit_to_app_rounded
+  ];
+
+  Widget userListTile(BuildContext context, String title, String subTitle, int index) {
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        splashColor: Theme.of(context).splashColor,
+        child: ListTile(
+          title: Text(title),
+          subtitle: Text(subTitle),
+          leading: Icon(_userTitleIcons[index]),
+          onTap: () {},
+        ),
+      ),
+    );
+  }
+
+  Widget userTitle(String title) {
+    return Text(
+      title,
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 23,
       ),
     );
   }
