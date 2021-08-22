5814c695435c2c206cfdc587b495c3cbb5acf113
nishi-2400 <a7w21087bump@yahoo.co.jp>
2021-08-22 17:49:19 +0900

    ナイトテーマ切り替え処理実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,31 @@
+import 'package:ecommerce/consts/theme_data.dart';
+import 'package:ecommerce/provider/dark_theme_provider.dart';
 import 'package:ecommerce/screens/bottom_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  DarkThemeProvider themeChangeNotifier = DarkThemeProvider();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: BottomBarScreen(),
+    return MultiProvider(
+        providers: [
+          // 子ウィジェットがDarkThemeProviderを利用できるようにする
+          ChangeNotifierProvider(create: (_) => themeChangeNotifier),
+        ],
+        // Consumer：notifyListeners()で通知された値を取得し、配下のウィジェットをリビルド
+        child: Consumer<DarkThemeProvider>(
+            builder: (context, themeData, child) {
+              return MaterialApp(
+                title: 'Flutter Demo',
+                theme: Styles.themeData(themeChangeNotifier.darkTheme, context),
+                home: BottomBarScreen(),
+              );
+            }),
     );
   }
 }
