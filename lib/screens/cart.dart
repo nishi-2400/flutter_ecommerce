dd1ffa8559291e506ad3af4ebd6491676d89a6c5
nishi-2400 <a7w21087bump@yahoo.co.jp>
2021-09-05 18:27:22 +0900

    カート画面作成

--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -1,11 +1,14 @@
 import 'package:ecommerce/screens/cart_empty.dart';
+import 'package:ecommerce/screens/cart_full.dart';
 import 'package:flutter/material.dart';
+
 
 class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    List products = [];
     return Scaffold(
-      body: CartEmpty(),
+      body: products.isEmpty ? CartEmpty() : CartFull(),
     );
   }
 }
